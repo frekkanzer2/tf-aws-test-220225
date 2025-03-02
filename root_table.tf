c0fcf8ce78fef6ae7d0ae1eb708dfab6485ba73b
Francesco Abate <abatefrancesco98@gmail.com>
2025-03-02 13:27:45 +0100

    subnets and root tables

--- root_table.tf
+++ root_table.tf
@@ -12,7 +12,7 @@ resource "aws_route_table" "aws-test-sab-pub-rt" {
   }
 }
 
-resource "aws_route_table" "aws-test-sab-pri-rt" {
+resource "aws_route_table" "aws-test-sab-pri-server-rt" {
   vpc_id = aws_vpc.aws-test-sab.id
 
   route {
@@ -21,7 +21,21 @@ resource "aws_route_table" "aws-test-sab-pri-rt" {
   }
 
   tags = {
-    Name = "aws-test-sab-pri-rt",
+    Name = "aws-test-sab-pri-server-rt",
+    Owner = var.owner
+  }
+}
+
+resource "aws_route_table" "aws-test-sab-pri-database-rt" {
+  vpc_id = aws_vpc.aws-test-sab.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.aws-test-sab-ngw.id
+  }
+
+  tags = {
+    Name = "aws-test-sab-pri-database-rt",
     Owner = var.owner
   }
 }
@@ -30,7 +44,11 @@ resource "aws_route_table_association" "aws-test-sab-pub-rt-ass" {
   subnet_id      = aws_subnet.aws-test-sab-pub.id
   route_table_id = aws_route_table.aws-test-sab-pub-rt.id
 }
-resource "aws_route_table_association" "aws-test-sab-pri-rt-ass" {
-  subnet_id      = aws_subnet.aws-test-sab-pri.id
-  route_table_id = aws_route_table.aws-test-sab-pri-rt.id
+resource "aws_route_table_association" "aws-test-sab-pri-server-rt-ass" {
+  subnet_id      = aws_subnet.aws-test-sab-pri-server.id
+  route_table_id = aws_route_table.aws-test-sab-pri-server-rt.id
+}
+resource "aws_route_table_association" "aws-test-sab-pri-database-rt-ass" {
+  subnet_id      = aws_subnet.aws-test-sab-pri-database.id
+  route_table_id = aws_route_table.aws-test-sab-pri-database-rt.id
 }
--- subnet.tf
+++ subnet.tf
@@ -8,12 +8,22 @@ resource "aws_subnet" "aws-test-sab-pub" {
   }
 }
 
-resource "aws_subnet" "aws-test-sab-pri" {
+resource "aws_subnet" "aws-test-sab-pri-server" {
   vpc_id     = aws_vpc.aws-test-sab.id
-  cidr_block = "10.1.0.32/27"
+  cidr_block = "10.1.0.32/28"
 
   tags = {
-    Name = "aws-test-sab-pri",
+    Name = "aws-test-sab-pri-server",
+    Owner = var.owner
+  }
+}
+
+resource "aws_subnet" "aws-test-sab-pri-database" {
+  vpc_id     = aws_vpc.aws-test-sab.id
+  cidr_block = "10.1.0.48/28"
+
+  tags = {
+    Name = "aws-test-sab-pri-database",
     Owner = var.owner
   }
 }
