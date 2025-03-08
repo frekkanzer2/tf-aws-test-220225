0038ad32e46d63df2afe94dfcf34a6e29830e91b
Francesco Abate <abatefrancesco98@gmail.com>
2025-03-08 16:42:32 +0100

    Added AZs to subnets

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "aws-test-sab-pub-1" {
   vpc_id     = aws_vpc.aws-test-sab.id
   cidr_block = "10.1.0.0/28"
+  availability_zone = "eu-south-1a"
 
   tags = {
     Name = "aws-test-sab-pub-1",
@@ -11,6 +12,7 @@ resource "aws_subnet" "aws-test-sab-pub-1" {
 resource "aws_subnet" "aws-test-sab-pub-2" {
   vpc_id     = aws_vpc.aws-test-sab.id
   cidr_block = "10.1.0.16/28"
+  availability_zone = "eu-south-1b"
 
   tags = {
     Name = "aws-test-sab-pub-2",
@@ -21,6 +23,7 @@ resource "aws_subnet" "aws-test-sab-pub-2" {
 resource "aws_subnet" "aws-test-sab-pri-server-1" {
   vpc_id     = aws_vpc.aws-test-sab.id
   cidr_block = "10.1.0.32/28"
+  availability_zone = "eu-south-1a"
 
   tags = {
     Name = "aws-test-sab-pri-server-1",
@@ -31,6 +34,7 @@ resource "aws_subnet" "aws-test-sab-pri-server-1" {
 resource "aws_subnet" "aws-test-sab-pri-database-1" {
   vpc_id     = aws_vpc.aws-test-sab.id
   cidr_block = "10.1.0.64/28"
+  availability_zone = "eu-south-1b"
 
   tags = {
     Name = "aws-test-sab-pri-database-1",
